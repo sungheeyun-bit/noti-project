366249c4e7204003918f318bd2a7c6529f914ebe
Seunghun Han <seunghunhan@Seunghunui-MacBookPro.local>
2021-04-14 13:14:46 +0900

    modified brandon task

---
+++ src/LandingPage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Template from "./AlarmListPage/Template";
+import ProductList from "./LandingPage/ProductList";
+import { AlarmProvider } from './AlarmContext';
+// import { ProductsProvider } from './AlarmContext';
+import SearchBar from './LandingPage/SearchBar';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+function LandingPage() {
+  return (  
+    <div>
+      <h1>랜딩페이지</h1>
+      <SearchBar/>
+      {/* <ProductsProvider> */}
+      <AlarmProvider>
+      <GlobalStyle />
+      <Template>
+        <ProductList />
+      </Template>
+      </AlarmProvider>
+      {/* </ProductsProvider> */}
+      
+    </div>
+    
+  
+    )
+}
+
+export default LandingPage;
